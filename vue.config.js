7ba084bb83eb995965cc89e2ef2ed551dbabec1c
IHIutch <jbhutch01@gmail.com>
2019-11-10 23:41:51 -0500

    add manifest and pwa info to vue config

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  pwa: {
+    name: "Fair Fines & Fees",
+    themeColor: "#4299e1",
+    msTileColor: "#4299e1",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black-translucent",
+    workboxOptions: {
+      skipWaiting: true
+    }
+  }
+};
