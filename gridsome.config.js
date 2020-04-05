b9ececf8ff07ee32d64390ddefc0cdd08d81a263
IHIutch <jbhutch01@gmail.com>
2020-04-05 18:43:24 -0400

    fix core-is issue

--- gridsome.config.js
+++ gridsome.config.js
@@ -22,16 +22,34 @@ module.exports = {
       options: {
         id: "UA-151711297-1",
         debug: {
-          sendHitTask: process.env.NODE_ENV === "production"
-        }
-      }
-    }
+          sendHitTask: process.env.NODE_ENV === "production",
+        },
+      },
+    },
+    {
+      use: "gridsome-plugin-pwa",
+      options: {
+        title: "Fair Fines No Fees",
+        startUrl: "/",
+        display: "standalone",
+        statusBarStyle: "default",
+        manifestPath: "./manifest.json",
+        serviceWorkerPath: "./service-worker.js",
+        shortName: "FairFinesNoFees",
+        themeColor: "#86b142",
+        backgroundColor: "#ffffff",
+        purpose: "maskable",
+        icon: "src/favicon.png", // must be supplied!
+        msTileImage: "src/favicon.png",
+        msTileColor: "#86b142",
+      },
+    },
   ],
   css: {
     loaderOptions: {
       postcss: {
-        plugins: postcssPlugins
-      }
-    }
-  }
+        plugins: postcssPlugins,
+      },
+    },
+  },
 };
